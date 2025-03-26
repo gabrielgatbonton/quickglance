8d061cb971f5cf6fc6112cc8b396d47de5f81743
melmatx <melpalana13@gmail.com>
2025-03-26 19:13:26 +0800

    Add styles for AddActionItem component to enhance layout and appearance

---
+++ components/add-action-item/styles.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    columnGap: 20,
+  },
+  indexContainer: {
+    backgroundColor: "gray",
+    width: "10%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 25,
+    height: 35,
+  },
+  indexText: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 18,
+  },
+  animatedContainer: {
+    flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "space-between",
+    borderRadius: 18,
+    overflow: "hidden",
+  },
+  label: {
+    color: "white",
+    fontSize: 15,
+  },
+});
+
+export default styles;
