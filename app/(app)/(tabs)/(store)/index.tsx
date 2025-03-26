4ee63ab88020f8f399f61b6c5f16304e1dd6e7d8
melmatx <melpalana13@gmail.com>
2025-03-26 19:12:46 +0800

    Add scroll-to-top functionality in Store component and update import paths

--- app/(app)/(tabs)/(store)/index.tsx
+++ app/(app)/(tabs)/(store)/index.tsx
@@ -1,18 +1,27 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { NativeSyntheticEvent, ScrollView } from "react-native";
 import SegmentedControl, {
   NativeSegmentedControlIOSChangeEvent,
 } from "@react-native-segmented-control/segmented-control";
 import globalStyles from "@/assets/global-styles";
 import StoreContent from "@/components/store-content";
-import { STORE_KEYS } from "@/utils/storeKeys";
+import { STORE_KEYS } from "@/constants/storeKeys";
 import styles from "./styles";
 import useSearch from "@/hooks/useSearch";
+import { useScrollToTop } from "@react-navigation/native";
+import { HEADER_HEIGHT } from "@/constants/navigation";
 
 const STORE_TABS = Object.values(STORE_KEYS);
 
 export default function Store() {
   const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const scrollViewRef = useRef<ScrollView>(null);
+  useScrollToTop(
+    useRef({
+      scrollToTop: () => scrollViewRef.current?.scrollTo({ y: -HEADER_HEIGHT }),
+    }),
+  );
 
   const search = useSearch();
 
@@ -20,6 +29,8 @@ export default function Store() {
     <ScrollView
       contentInsetAdjustmentBehavior="automatic"
       contentContainerStyle={[globalStyles.container, { paddingTop: 0 }]}
+      scrollToOverflowEnabled
+      ref={scrollViewRef}
     >
       <SegmentedControl
         values={STORE_TABS}
