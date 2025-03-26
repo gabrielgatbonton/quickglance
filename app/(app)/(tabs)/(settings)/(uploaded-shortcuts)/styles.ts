615f8690e01c2d2d611efde1334ab9ca8c5789be
melmatx <melpalana13@gmail.com>
2025-03-26 19:12:27 +0800

    Remove background color from uploaded shortcuts styles for improved design consistency

--- app/(app)/(tabs)/(settings)/(uploaded-shortcuts)/styles.ts
+++ app/(app)/(tabs)/(settings)/(uploaded-shortcuts)/styles.ts
@@ -2,7 +2,6 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   contentContainer: {
-    backgroundColor: "white",
     borderRadius: 10,
     overflow: "hidden",
   },
