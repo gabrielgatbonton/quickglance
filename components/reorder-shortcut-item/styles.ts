380324e6d6ae421cdd905e746a1444543a288591
melmatx <melpalana13@gmail.com>
2025-03-26 19:15:10 +0800

    Add styles for ReorderShortcutItem component to define layout and appearance

---
+++ components/reorder-shortcut-item/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderRadius: 18,
+    overflow: "hidden",
+  },
+  contentContainer: {
+    justifyContent: "space-between",
+    height: "100%",
+  },
+  label: {
+    color: "white",
+    fontSize: 15,
+  },
+});
+
+export default styles;
