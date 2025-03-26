7d52cb3f2ddc1011fdd634f6cc01ecf2ac835644
melmatx <melpalana13@gmail.com>
2025-03-26 19:13:56 +0800

    Add styles for CustomColorPicker component to enhance layout and appearance

---
+++ components/custom-color-picker/styles.ts
@@ -0,0 +1,87 @@
+import { Colors } from "@/assets/colors";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  pickerContainer: {
+    alignSelf: "center",
+    width: 300,
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+
+    elevation: 10,
+  },
+  panelStyle: {
+    borderRadius: 16,
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  sliderStyle: {
+    borderRadius: 20,
+    marginTop: 20,
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  previewTxtContainer: {
+    paddingTop: 20,
+    marginTop: 20,
+    borderTopWidth: 1,
+    borderColor: "lightgray",
+  },
+  previewTxt: {
+    color: Colors.SECONDARY,
+    fontSize: 16,
+  },
+  swatchesContainer: {
+    paddingTop: 20,
+    marginTop: 20,
+    borderTopWidth: 1,
+    borderColor: "lightgray",
+    alignItems: "center",
+    flexWrap: "nowrap",
+    gap: 10,
+  },
+  swatchStyle: {
+    borderRadius: 20,
+    height: 30,
+    width: 30,
+    margin: 0,
+    marginBottom: 0,
+    marginHorizontal: 0,
+    marginVertical: 0,
+  },
+  buttonContainer: {
+    alignSelf: "center",
+    padding: 30,
+  },
+});
+
+export default styles;
