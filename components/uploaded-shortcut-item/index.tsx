56517eb13074fae3d79a365d8cd2fe084d857b97
melmatx <melpalana13@gmail.com>
2025-03-26 19:16:04 +0800

    Refactor UploadedShortcutItem component to use constants for types and improve Pressable styling

--- components/uploaded-shortcut-item/index.tsx
+++ components/uploaded-shortcut-item/index.tsx
@@ -1,16 +1,16 @@
-import { Shortcut } from "@/utils/types";
+import { Shortcut } from "@/constants/types";
 import { SymbolView } from "expo-symbols";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, Text } from "react-native";
 import styles from "./styles";
 import pressedBgColor from "@/utils/pressedBgColor";
 
 export default function UploadedShortcutItem({ item }: { item: Shortcut }) {
   return (
-    <Pressable style={({ pressed }) => pressedBgColor({ pressed })}>
-      <View style={styles.container}>
-        <Text>{item.name}</Text>
-        <SymbolView name="chevron.right" size={12} tintColor="gray" />
-      </View>
+    <Pressable
+      style={({ pressed }) => [styles.container, pressedBgColor({ pressed })]}
+    >
+      <Text>{item.name}</Text>
+      <SymbolView name="chevron.right" size={12} tintColor="gray" />
     </Pressable>
   );
 }
