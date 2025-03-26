b01a342b084331ea584fb7b129a3e8ef7c59d5b2
melmatx <melpalana13@gmail.com>
2025-03-26 19:13:38 +0800

    Add CustomButton component for reusable button functionality

---
+++ components/custom-button/index.tsx
@@ -0,0 +1,47 @@
+import pressedOpacity from "@/utils/pressedOpacity";
+import {
+  Pressable,
+  PressableProps,
+  StyleProp,
+  Text,
+  TextStyle,
+  View,
+  ViewStyle,
+} from "react-native";
+import styles from "./styles";
+import { Colors } from "@/assets/colors";
+
+type CustomButtonProps = PressableProps & {
+  title: string;
+  color?: string;
+  containerStyle?: StyleProp<ViewStyle>;
+  textStyle?: StyleProp<TextStyle>;
+};
+
+export default function CustomButton({
+  title,
+  color = Colors.PRIMARY,
+  onPress,
+  disabled,
+  containerStyle = {},
+  textStyle = {},
+  ...buttonProps
+}: CustomButtonProps) {
+  return (
+    <Pressable
+      onPress={onPress}
+      disabled={disabled}
+      style={({ pressed }) => [
+        pressedOpacity({ pressed }),
+        disabled && { opacity: 0.5 },
+      ]}
+      {...buttonProps}
+    >
+      <View
+        style={[styles.container, { backgroundColor: color }, containerStyle]}
+      >
+        <Text style={[styles.title, textStyle]}>{title}</Text>
+      </View>
+    </Pressable>
+  );
+}
