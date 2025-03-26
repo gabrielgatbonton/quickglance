d141b08212a8cc202515bf8318b0dc180d7c862c
melmatx <melpalana13@gmail.com>
2025-03-26 19:16:26 +0800

    Add SAMPLE_AUTOMATION constant for predefined automation scenarios

---
+++ constants/sampleAutomation.ts
@@ -0,0 +1,14 @@
+import { Automation } from "./types";
+
+export const SAMPLE_AUTOMATION: Automation[] = [
+  {
+    id: "1",
+    name: "When the user feels sad 😢",
+    actions: ["Play 🎶 My Upbeat Mix Playlist", "Send Message to 💬 Mom"],
+  },
+  {
+    id: "2",
+    name: "When the user feels scared 😢",
+    actions: ["Call Emergency Services", "Navigate to Doctor"],
+  },
+];
