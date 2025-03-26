2ea76a7123c11486b83f9f18a1024653c581f0d5
melmatx <melpalana13@gmail.com>
2025-03-26 19:15:36 +0800

    Refactor ShortcutCategoryItem to use constants for Category type and add onCategoryPress prop for handling category presses

--- components/shortcut-category-item/index.tsx
+++ components/shortcut-category-item/index.tsx
@@ -1,13 +1,22 @@
 import pressedOpacity from "@/utils/pressedOpacity";
-import { Category } from "@/utils/types";
+import { Category } from "@/constants/types";
 import { BlurView } from "expo-blur";
 import { ImageBackground, Pressable, Text } from "react-native";
 import styles from "./styles";
 
-export default function ShortcutCategoryItem({ item }: { item: Category }) {
+type ShortcutCategoryItemProps = {
+  item: Category;
+  onCategoryPress?: (category: Category) => void;
+};
+
+export default function ShortcutCategoryItem({
+  item,
+  onCategoryPress,
+}: ShortcutCategoryItemProps) {
   return (
     <Pressable
       style={({ pressed }) => pressedOpacity({ pressed, opacity: 0.6 })}
+      onPress={() => onCategoryPress?.(item)}
     >
       <ImageBackground source={{ uri: item.id }} style={styles.imageContainer}>
         <BlurView intensity={50} tint="dark" style={styles.nameContainer}>
