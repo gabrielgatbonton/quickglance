0e6545421f248ccb257bba896c7932907f6c1e9f
melmatx <melpalana13@gmail.com>
2025-03-26 19:11:28 +0800

    Refactor styles for add shortcut screen with improved layout and spacing

--- app/(app)/(add-shortcut)/styles.ts
+++ app/(app)/(add-shortcut)/styles.ts
@@ -4,24 +4,45 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
   },
-  dragDropView: {
-    width: "100%",
-    height: "60%",
-  },
-  dropContainer: {
+  editDetailsContainer: {
     flex: 1,
+    padding: 20,
+    rowGap: 15,
+  },
+  inputsContainer: {
+    backgroundColor: "white",
+    borderRadius: 10,
+  },
+  hintContainer: {
+    paddingHorizontal: 10,
+  },
+  hint: {
+    color: Colors.SECONDARY,
+    fontSize: 12,
+  },
+  gradientsContainer: {
+    paddingVertical: 10,
+    rowGap: 15,
+  },
+  shortcutsContainer: {
+    width: "100%",
+    height: "63%",
+  },
+  noShortcutsContainer: {
+    flex: 1,
+    margin: 20,
     borderWidth: 3,
     borderRadius: 18,
     borderStyle: "dashed",
     borderColor: Colors.SECONDARY,
     backgroundColor: Colors.BACKGROUND,
     overflow: "visible",
-    height: "100%",
-    width: "100%",
     justifyContent: "center",
     alignItems: "center",
+  },
+  shortcutsGrid: {
+    padding: 20,
   },
   dropText: {
     color: "gray",
@@ -31,14 +52,20 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-around",
+    paddingTop: 10,
+    paddingBottom: 20,
   },
   sheetHeaderTitle: {
     fontWeight: "500",
-    fontSize: 16,
+    fontSize: 18,
   },
   contentContainer: {
     columnGap: 15,
-    padding: 20,
+    rowGap: 15,
+    paddingHorizontal: 15,
+  },
+  columnWrapper: {
+    columnGap: 10,
   },
 });
 
