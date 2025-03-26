0d8ecabc39ee56e34fbf006f99be3e640f91ffd0
melmatx <melpalana13@gmail.com>
2025-03-26 19:13:44 +0800

    Add styles for CustomButton component to improve appearance and layout

---
+++ components/custom-button/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 14,
+    paddingHorizontal: 20,
+    borderRadius: 12,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
+
+export default styles;
