1e0d7bfa3b3ed3e0d5aa23353209fceb8536f21e
melmatx <melpalana13@gmail.com>
2025-04-01 03:17:56 +0800

    Add styles for change password modal

---
+++ app/(app)/(modal)/(change-password)/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    rowGap: 20,
+    padding: 20,
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 16,
+    paddingHorizontal: 14,
+    borderRadius: 13,
+  },
+  inputError: {
+    borderColor: "red",
+    borderWidth: 1,
+  },
+});
+
+export default styles;
