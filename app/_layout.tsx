521d19df03446ce7e5c7c22972e3ae21270da0b8
melmatx <melpalana13@gmail.com>
2025-04-01 03:19:22 +0800

    Enhance RootLayout to integrate QueryClient and KeyboardProvider for improved state management and keyboard handling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Slot } from "expo-router";
+import { KeyboardProvider } from "react-native-keyboard-controller";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { configureReanimatedLogger } from "react-native-reanimated";
+
+export const queryClient = new QueryClient();
+
+configureReanimatedLogger({
+  strict: false,
+});
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView>
-      <Slot />
-    </GestureHandlerRootView>
+    <QueryClientProvider client={queryClient}>
+      <GestureHandlerRootView>
+        <KeyboardProvider>
+          <Slot />
+        </KeyboardProvider>
+      </GestureHandlerRootView>
+    </QueryClientProvider>
   );
 }
