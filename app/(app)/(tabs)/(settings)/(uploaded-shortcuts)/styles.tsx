7164bf7772dbeb318e3e77be6afb2ab48912ac6a
melmatx <melpalana13@gmail.com>
2025-04-01 03:15:38 +0800

    Add styles for UploadedShortcuts component

---
+++ app/(app)/(tabs)/(settings)/(uploaded-shortcuts)/styles.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+});
+
+export default styles;
