9209f0296fc72ae1596c900ee3d6106ef530d7aa
melmatx <melpalana13@gmail.com>
2025-04-01 03:13:57 +0800

    Add styles for InputErrorView component

---
+++ components/input-error-view/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  errorContainer: {
+    rowGap: 5,
+    paddingTop: 5,
+    paddingHorizontal: 10,
+  },
+  errorText: {
+    color: "red",
+  },
+});
+
+export default styles;
