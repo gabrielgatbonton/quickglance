fc8a38dc05521ee4728054e2ae140dc599905ef8
melmatx <melpalana13@gmail.com>
2025-04-01 03:19:17 +0800

    Refactor RootLayout to include authentication token handling and update modal screen options

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,27 +1,58 @@
+import useAuthStore, {
+  AuthStoreActions,
+  AuthStoreState,
+} from "@/stores/useAuthStore";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { useShallow } from "zustand/react/shallow";
 
 export default function RootLayout() {
+  const { token, refreshAuth } = useAuthStore<{
+    token: AuthStoreState["token"];
+    refreshAuth: AuthStoreActions["refreshAuth"];
+  }>(
+    useShallow((state) => ({
+      token: state.token,
+      refreshAuth: state.refreshAuth,
+    })),
+  );
+
+  // Restore token on app start
+  useEffect(() => {
+    refreshAuth();
+  }, [refreshAuth]);
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" />
       <Stack.Screen
-        name="(welcome)"
+        name="(modal)/(welcome)"
         options={{
-          title: "Welcome",
+          presentation: "modal",
+          gestureEnabled: Boolean(token),
+        }}
+      />
+      <Stack.Screen
+        name="(modal)/(add-shortcut)"
+        options={{
           presentation: "modal",
         }}
       />
       <Stack.Screen
-        name="(add-shortcut)"
+        name="(modal)/(reorder-shortcuts)"
         options={{
-          title: "Add Shortcut",
           presentation: "modal",
         }}
       />
       <Stack.Screen
-        name="(reorder-shortcuts)"
+        name="(modal)/(edit-profile)"
         options={{
-          title: "Reorder Shortcuts",
+          presentation: "modal",
+        }}
+      />
+      <Stack.Screen
+        name="(modal)/(change-password)"
+        options={{
           presentation: "modal",
         }}
       />
