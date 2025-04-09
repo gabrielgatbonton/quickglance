b076d87d3012803eaf02aa2c4dfa29de763f2e4b
melmatx <melpalana13@gmail.com>
2025-04-09 17:25:49 +0800

    Add AddAutomationLayout component with navigation and header options

---
+++ app/(app)/(modal)/(add-automation)/_layout.tsx
@@ -0,0 +1,41 @@
+import { Colors } from "@/assets/colors";
+import CustomLink from "@/components/custom-link";
+import { DEFAULT_FONT_FAMILY } from "@/components/custom-text/styles";
+import { router, Stack } from "expo-router";
+
+export default function AddAutomationLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerTintColor: Colors.PRIMARY,
+        headerTitleStyle: { color: "black", fontFamily: DEFAULT_FONT_FAMILY },
+        headerBackTitleStyle: { fontFamily: DEFAULT_FONT_FAMILY },
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Add Automation",
+          headerLeft: () => (
+            <CustomLink
+              title="Cancel"
+              onPress={() => router.back()}
+              color={Colors.PRIMARY}
+            />
+          ),
+          headerRight: () => (
+            <CustomLink
+              title="Done"
+              bold
+              onPress={() => {
+                router.back();
+                console.log("Automation saved!");
+              }}
+              color={Colors.PRIMARY}
+            />
+          ),
+        }}
+      />
+    </Stack>
+  );
+}
