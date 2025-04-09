6d33c7a0828b026e6289afbd264b4056a7ec2bdf
melmatx <melpalana13@gmail.com>
2025-04-09 17:24:22 +0800

    Implement authentication check in TabIndex component to redirect users based on token presence

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -1,5 +1,28 @@
+import useAuthStore from "@/stores/useAuthStore";
 import { Redirect } from "expo-router";
+import { ImageBackground, StyleSheet } from "react-native";
 
 export default function TabIndex() {
-  return <Redirect href="/(home)" />;
+  const token = useAuthStore((state) => state.token);
+
+  if (token) {
+    return <Redirect href="/(home)" />;
+  }
+
+  return (
+    <ImageBackground
+      source={{ uri: "mesh-gradient" }}
+      style={styles.container}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  logo: {
+    aspectRatio: 1,
+    width: "50%",
+  },
+});
